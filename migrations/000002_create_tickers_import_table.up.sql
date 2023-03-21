49eafa47b032da1e620ad9a6b93f1608c842226e
garma <garma86@mail.ru>
2023-03-21 17:27:47 +0800

    Changed second migration. Removed id column, since it is not needed

--- migrations/000002_create_tickers_import_table.up.sql
+++ migrations/000002_create_tickers_import_table.up.sql
@@ -1,9 +1,8 @@
 create table public.tickers_import (
-	id bigserial primary key,
 	secid     text,
 	shortname text,
 	boardid   text,
 	Tradedate timestamp,
 	Volume    bigint,
-	unique (secid, boardid)
+	primary key (secid, boardid)
 );
